3d5168eaa9e877c1cd2ab3a3c441a6633e9e28ae
Assukumar <assuprusty@gmail.com>
2024-10-10 14:59:01 +0530

    First Commit

---
+++ MavenCmd3/src/test/java/pac1/OrgTest.java
@@ -0,0 +1,30 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute create organization test");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modify organization test");
+	}
+
+	@Test
+	public void editOrgTest() {
+		System.out.println("execute modify organization test");
+	}
+
+}
---
+++ MavenCmd3/src/test/java/pac3/CreateUserTest.java
@@ -0,0 +1,10 @@
+package pac3;
+
+import org.testng.annotations.Test;
+
+public class CreateUserTest {
+  @Test
+  public void m1Test() {
+	  System.out.println("Created");
+  }
+}
